0f2e7ea166df6363623f1ac7895fd2018f18f765
Oliver Zenger <oliver@zenger.org>
2024-05-20 19:24:29 +0200

    Add AstroPizzaBackendApplicationTests class for testing application context loading

---
+++ src/test/java/bbw/ch/zms/astropizzabackend/AstroPizzaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bbw.ch.zms.astropizzabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroPizzaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
