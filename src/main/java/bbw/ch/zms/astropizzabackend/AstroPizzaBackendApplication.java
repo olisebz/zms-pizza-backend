ea4735b04fc19fd65944ec460ad2e96459556ada
Oliver Zenger <oliver@zenger.org>
2024-05-20 19:24:17 +0200

    feat: Add AstroPizzaBackendApplication class

---
+++ src/main/java/bbw/ch/zms/astropizzabackend/AstroPizzaBackendApplication.java
@@ -0,0 +1,13 @@
+package bbw.ch.zms.astropizzabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroPizzaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroPizzaBackendApplication.class, args);
+	}
+
+}
