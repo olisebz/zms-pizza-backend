79eb5f1f5b06ff74a8b51d63f132b71148629e9d
Oliver Zenger <oliver@zenger.org>
2024-05-21 11:41:51 +0200

    chore: Add ZmsPizzaBackendApplicationTests class for testing ZmsPizzaBackendApplication

---
+++ src/test/java/yando/zms/zmspizzabackend/ZmsPizzaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package yando.zms.zmspizzabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZmsPizzaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
