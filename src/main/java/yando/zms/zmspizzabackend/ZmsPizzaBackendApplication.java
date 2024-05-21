77a2a68216018ad4174e46c262f2af89d9f959c1
Oliver Zenger <oliver@zenger.org>
2024-05-21 11:41:46 +0200

    chore: Add ZmsPizzaBackendApplication class with main method

---
+++ src/main/java/yando/zms/zmspizzabackend/ZmsPizzaBackendApplication.java
@@ -0,0 +1,14 @@
+package yando.zms.zmspizzabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmsPizzaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZmsPizzaBackendApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
