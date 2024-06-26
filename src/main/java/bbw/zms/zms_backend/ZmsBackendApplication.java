119c1d63e6f3801dd43939009ff299701709b35b
Zenger Oliver <oliver.zenger@swissom.com>
2024-06-26 16:43:41 +0200

    chore: Add ZmsBackendApplication class with main method

---
+++ src/main/java/bbw/zms/zms_backend/ZmsBackendApplication.java
@@ -0,0 +1,13 @@
+package bbw.zms.zms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZmsBackendApplication.class, args);
+	}
+
+}
