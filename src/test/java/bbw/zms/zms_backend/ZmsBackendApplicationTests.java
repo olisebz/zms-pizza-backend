af4327ba9fb8807abb4a972463df6725506ba122
Zenger Oliver <oliver.zenger@swissom.com>
2024-06-26 16:43:49 +0200

    chore: Add ZmsBackendApplicationTests class with contextLoads test method

---
+++ src/test/java/bbw/zms/zms_backend/ZmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bbw.zms.zms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
